a864a811e56f527149cc8b6a727b0cea55c5d8be
IcePanorama <thriftedroot@protonmail.com>
2025-03-11 11:45:13 -0400

    Refactored code.

--- src/main.c
+++ src/main.c
@@ -17,6 +17,8 @@ main (int argc, char **argv)
   // FIXME: assuming argv[1] is an obj file for the time being.
   ObjFile_t obj;
   int status = create_obj_file_from_file (&obj, argv[1]);
+  if (status != 0)
+    goto prg_exit;
 
   struct VertexCoord_s centroid = calc_centroid_from_obj (&obj);
   LOG_DEBUG_INFO ("Centroid: (%f, %f, %f, %f)\n", centroid.x, centroid.y,
@@ -26,6 +28,7 @@ main (int argc, char **argv)
   calc_covar_mat_from_obj_centroid (&obj, &centroid, covar_mat);
   print_covar_mat (covar_mat);
 
+prg_exit:
   free_obj_file (&obj);
   return status == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
