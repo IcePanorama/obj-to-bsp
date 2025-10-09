17eb2ecf28fa098befaf381ba0b3f35c474ac293
IcePanorama <thriftedroot@protonmail.com>
2025-10-09 13:28:04 -0400

    Refactored code.

--- src/bsp/tree.c
+++ src/bsp/tree.c
@@ -14,10 +14,14 @@ struct BSPTree_s
   void *tmp;
 };
 
+/**
+ *  Given a face `f`, this writes its 3 vertices from `v` into out. This
+ *  function assumes out is of length 3.
+ */
 static int
-get_face_verts (_OBJFace_t *f, DynamicArr_t *v, float *out)
+get_face_verts (_OBJFace_t *f, DynamicArr_t *v, float out[static 1])
 {
-  if (!f)
+  if (!f || !v)
     return -1;
 
   size_t *idx = objf_get_vert_idxs (f);
@@ -36,52 +40,10 @@ get_face_verts (_OBJFace_t *f, DynamicArr_t *v, float *out)
   return 0;
 }
 
-/*
-static int
-calc_obj_centroid (_OBJObj_t *o, float *out)
-{
-  DynamicArr_t *faces = objo_get_faces (o);
-  DynamicArr_t *verts = objo_get_verts (o);
-  if ((!faces) || (!verts))
-    return -1;
-
-  size_t n_faces = DynA_get_size (faces);
-  for (size_t i = 0; i < n_faces; i++)
-    {
-      _OBJFace_t *f = (_OBJFace_t *)DynA_at (faces, i);
-      if (!f)
-        {
-          fprintf (stderr, "%s: Error fetching face at idx %zu", __func__, i);
-          return -1;
-        }
-
-      float f_cent[3] = { 0 };
-      if (get_face_verts (f, verts, f_cent) != 0)
-        {
-          fprintf (stderr, "%s: error calculating centroid of face #%zu\n",
-                   __func__, i);
-          return -1;
-        }
-
-      out[0] += f_cent[0];
-      out[1] += f_cent[1];
-      out[2] += f_cent[2];
-    }
-
-  out[0] /= n_faces * 3;
-  out[1] /= n_faces * 3;
-  out[2] /= n_faces * 3;
-
-  return 0;
-}
-*/
-
-float
-dist (float x0, float y0, float z0, float x1, float y1, float z1)
-{
-  return sqrtf (powf (x1 - x0, 2) + powf (y1 - y0, 2) + powf (z1 - z0, 2));
-}
-
+/**
+ *  Calculates a normal, n, of face `f` using its vertices from `v`. Assumes
+ *  len of `n` is at least 3.
+ */
 static int
 calc_face_norm (_OBJFace_t *f, DynamicArr_t *v, float n[static 1])
 {
@@ -92,30 +54,27 @@ calc_face_norm (_OBJFace_t *f, DynamicArr_t *v, float n[static 1])
   if (!idx)
     return -1;
 
-  float fverts[3][3] = { 0 };
+  float c[3][3] = { 0 };
   for (size_t j = 0; j < 3; j++)
     {
       _OBJVertexCoord_t *curr_v = (_OBJVertexCoord_t *)DynA_at (v, idx[j]);
       if (!curr_v)
         return -1;
 
-      fverts[j][0] = objv_get_x (curr_v);
-      fverts[j][1] = objv_get_y (curr_v);
-      fverts[j][2] = objv_get_z (curr_v);
+      c[j][0] = objv_get_x (curr_v);
+      c[j][1] = objv_get_y (curr_v);
+      c[j][2] = objv_get_z (curr_v);
     }
 
   /**
    *  See: https://wikis.khronos.org/opengl/Calculating_a_Surface_Normal.
    */
-  float u[3] = { fverts[1][0] - fverts[0][0], fverts[1][1] - fverts[0][1],
-                 fverts[1][2] - fverts[0][2] };
+  float e0[3] = { c[1][0] - c[0][0], c[1][1] - c[0][1], c[1][2] - c[0][2] };
+  float e1[3] = { c[2][0] - c[0][0], c[2][1] - c[0][1], c[2][2] - c[0][2] };
 
-  float _v[3] = { fverts[2][0] - fverts[0][0], fverts[2][1] - fverts[0][1],
-                  fverts[2][2] - fverts[0][2] };
-
-  n[0] = (u[1] * _v[2]) - (u[2] * _v[1]);
-  n[1] = (u[2] * _v[0]) - (u[0] * _v[2]);
-  n[2] = (u[0] * _v[1]) - (u[1] * _v[0]);
+  n[0] = (e0[1] * e1[2]) - (e0[2] * e1[1]);
+  n[1] = (e0[2] * e1[0]) - (e0[0] * e1[2]);
+  n[2] = (e0[0] * e1[1]) - (e0[1] * e1[0]);
 
   float len = sqrt (n[0] * n[0] + n[1] * n[1] + n[2] * n[2]);
 
@@ -126,9 +85,50 @@ calc_face_norm (_OBJFace_t *f, DynamicArr_t *v, float n[static 1])
   return 0;
 }
 
-// FIXME: need to rename this func
+static uint32_t
+score_split (float o[static 1], float n[static 1], size_t idx, DynamicArr_t *f,
+             DynamicArr_t *v)
+{
+  if (!f || !v)
+    return UINT32_MAX;
+
+  int32_t count = 0;
+  const size_t N_FACES = DynA_get_size (f);
+  for (size_t i = 0; i < N_FACES; i++)
+    {
+      if (idx == i)
+        continue;
+
+      _OBJFace_t *curr = (_OBJFace_t *)DynA_at (f, i);
+      if (!curr)
+        return UINT32_MAX;
+
+      float end[3] = { 0 };
+      if (get_face_verts (curr, v, end) != 0)
+        return UINT32_MAX;
+
+      end[0] /= 3;
+      end[1] /= 3;
+      end[2] /= 3;
+
+      float diff[3] = {
+        end[0] - o[0],
+        end[1] - o[1],
+        end[2] - o[2],
+      };
+
+      float dist = n[0] * diff[0] + n[1] * diff[1] + n[2] * diff[2];
+      if (dist < 0)
+        count -= 1;
+      else
+        count += 1;
+    }
+
+  return (uint32_t)sqrt (count * count);
+}
+
 _OBJFace_t *
-split_objo (_OBJObj_t *o)
+find_splitting_plane (_OBJObj_t *o)
 {
   DynamicArr_t *faces = objo_get_faces (o);
   DynamicArr_t *verts = objo_get_verts (o);
@@ -148,8 +148,6 @@ split_objo (_OBJObj_t *o)
       if (calc_face_norm (curr, verts, norm) != 0)
         return NULL;
 
-      // printf ("Norm: %f, %f, %f\n", norm[0], norm[1], norm[2]);
-
       float origin[3] = { 0 };
       if (get_face_verts (curr, verts, origin) != 0)
         return NULL;
@@ -158,48 +156,8 @@ split_objo (_OBJObj_t *o)
       origin[1] /= 3;
       origin[2] /= 3;
 
-      int32_t count = 0;
-      for (size_t j = 0; j < N_FACES; j++)
-        {
-          if (i == j)
-            continue;
-
-          // printf ("i: %zu, j: %zu\n", i, j);
-          _OBJFace_t *tmp = (_OBJFace_t *)DynA_at (faces, j);
-          if (!tmp)
-            return NULL;
-
-          float end[3] = { 0 };
-          if (get_face_verts (tmp, verts, end) != 0)
-            return NULL;
-
-          end[0] /= 3;
-          end[1] /= 3;
-          end[2] /= 3;
-
-          /*
-          printf ("origin: %f, %f, %f\n", origin[0], origin[1], origin[2]);
-          printf ("end: %f, %f, %f\n", end[0], end[1], end[2]);
-          */
-
-          float diff[3] = {
-            end[0] - origin[0],
-            end[1] - origin[1],
-            end[2] - origin[2],
-          };
-          // printf ("diff: %f, %f, %f\n", diff[0], diff[1], diff[2]);
-
-          float dist
-              = norm[0] * diff[0] + norm[1] * diff[1] + norm[2] * diff[2];
-          // printf ("Dist: %f\n", dist);
-
-          if (dist < 0)
-            count -= 1;
-          else
-            count += 1;
-        }
-
-      if (sqrt (count * count) < min_count)
+      uint32_t count = score_split (origin, norm, i, faces, verts);
+      if (count < min_count)
         {
           printf ("New min count: %d\n", count);
           printf ("Old: %d\n", min_count);
@@ -224,14 +182,8 @@ bsp_alloc (OBJFile_t *o)
     {
       _OBJObj_t *curr = *(_OBJObj_t **)DynA_at (objs, i);
 
-      /*
-      float c[3] = { 0 };
-      if (calc_obj_centroid (curr, c) != 0)
-        return NULL;
-        */
-
-      _OBJFace_t *c_face = split_objo (curr);
-      if (!c_face)
+      _OBJFace_t *splitting_plane = find_splitting_plane (curr);
+      if (!splitting_plane)
         return NULL;
 
       break;
